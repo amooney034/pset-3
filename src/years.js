bdcef95b20b8e4d4b05b279a557138a1203b82d3
Aidan Mooney <amooney034@gmail.com>
2019-11-04 10:05:15 -0500

    years.js

---
+++ src/years.js
@@ -0,0 +1,22 @@
+const readlineSync = require("readline-sync");
+const MIN = 0;
+const MAX = Number.MAX_SAFE_INTEGER;
+
+const enter = Number(readlineSync.question("\nEnter a year: "));
+
+
+if (enter < MIN || enter > MAX) {
+  console.log("\nInvalid.\n");
+}
+else if (enter % 4 === 0) {
+  console.log("\n" + enter + " is a leap year.\n");
+}
+else if (Number.isNaN(enter)) {
+  console.log("\nInvalid.\n");
+}
+else if (!(Number.isInteger(enter))) {
+  console.log("\nInvalid.\n");
+}
+else {
+  console.log("\n" + enter + " is not a leap year.\n");
+}
