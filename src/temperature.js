0cc4cb32c502415f4584f73acebd43398c6b86c3
Aidan Mooney <amooney034@gmail.com>
2019-11-04 11:04:33 -0500

    final final monday

--- src/temperature.js
+++ src/temperature.js
@@ -4,9 +4,35 @@ const MIN = Number.MIN_SAFE_INTEGER
 const MAX = Number.MAX_SAFE_INTEGER;
 const temp = Number(readlineSync.question("\nEnter a temperature: "));
 const scale = String(readlineSync.question("Enter a scale: "));
-const tempUp = temp.toUpperCase();
-const scaleUp = scale.toUpperCase();
+const scaleUp = scale.toUpperCase()
 
-if (tempUp <= 32 && scaleUp == "F") {
+if (temp <= 32 && scaleUp == "F") {
   console.log("\nSolid.\n");
+}
+else if ((temp > 32 && temp < 212) && scaleUp == "F") {
+  console.log("\nLiquid.\n");
+}
+else if (temp >= 212 && scaleUp == "F") {
+  console.log("\nGas.\n");
+}
+else if (temp <= 0 && scaleUp == "C") {
+  console.log("\nSolid.\n");
+}
+else if ((temp > 0 && temp < 100) && scaleUp == "C") {
+  console.log("\nLiquid.\n");
+}
+else if (temp >= 100 && scaleUp == "C") {
+  console.log("\nGas.\n");
+}
+else if (temp <= 273.2 && scaleUp == "K") {
+  console.log("\nSolid.\n");
+}
+else if ((temp > 273.2 && temp < 373.2) && scaleUp == "K") {
+  console.log("\nLiquid.\n");
+}
+else if (temp >= 373.2 && scaleUp == "K") {
+  console.log("\nGas.\n");
+}
+else {
+  console.log("\nInvalid.\n");
 }
