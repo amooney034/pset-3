a754ab9860bbc16d498421e3384df28003f2fac2
Aidan Mooney <amooney034@gmail.com>
2019-11-04 09:36:28 -0500

    grades.js

--- src/grades.js
+++ src/grades.js
@@ -1,6 +1,24 @@
 const readlineSync = require("readline-sync");
-
 const MIN = 0;
 const MAX = 100;
 
-const a = 
+const enter = Number(readlineSync.question("\nEnter a grade: "));
+
+if (enter < MIN || enter > MAX) {
+  console.log("\nInvalid.\n");
+}
+else if (enter < 101 && enter > 89) {
+  console.log("\nYou received a(n) A.\n");
+}
+else if (enter < 90 && enter > 79) {
+  console.log("\nYou received a(n) B.\n");
+}
+else if (enter < 80 && enter > 69) {
+  console.log("\nYou received a(n) C.\n");
+}
+else if (enter < 70 && enter > 59) {
+  console.log("\nYou received a(n) D.\n");
+}
+else {
+  console.log("\nYou received a(n) F.\n");
+}
