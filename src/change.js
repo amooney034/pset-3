748335e8917d69d151a0832bf449327a40c7f466
Aidan Mooney <amooney034@gmail.com>
2019-11-06 09:55:56 -0500

    heck yeah

--- src/change.js
+++ src/change.js
@@ -2,6 +2,27 @@ const readlineSync = require("readline-sync");
 
 const MIN = 0.01;
 const MAX = 0.99;
+const amountQuarter = 0.25;
+const amountDime = 0.10;
+const amountNickel = 0.05;
+const amountPenny = 0.01;
 const enter = Number(readlineSync.question("\nEnter a dollar amount: "));
 
-if
+let quarter = Math.floor(enter/amountQuarter);
+let finalQuarter = enter-(quarter)*(amountQuarter);
+let dime = Math.floor(finalQuarter/amountDime);
+let finalDime = finalQuarter-(dime)*(amountDime);
+let nickel = Math.floor(finalDime/amountNickel);
+let finalNickel = finalDime-(nickel)*(amountNickel);
+let penny = Math.round(finalNickel/amountPenny);
+
+
+if (Number.isNaN(enter)) {
+    console.log("\nInvalid\n")
+}
+else if (enter < MIN || enter > MAX) {
+    console.log("\nInvalid\n")
+}
+else {
+    console.log("\n" + quarter + " quarters, " + dime + " dimes, " + nickel + " nickels" + ", and " + penny + " pennies.\n")
+}
--- src/sequence.js
+++ src/sequence.js
@@ -1,5 +1,31 @@
 const readlineSync = require("readline-sync");
 
-const MIN = Number.MIN_SAFE_INTEGER;
-const MAX = Number.MAX_SAFE_INTEGER;
-const enter = Number(readlineSync.question("\nEnter three numbers. "));
+console.log("\nEnter three numbers. \n");
+const firstNumber = Number(readlineSync.question("\n"));
+const secondNumber = Number(readlineSync.question(""));
+const thirdNumber = Number(readlineSync.question(""));
+
+if (Number.isNaN(firstNumber) || Number.isNaN(secondNumber) || Number.isNaN(thirdNumber)) {
+    console.log("\nInvalid.");
+}
+else if (firstNumber < Number.MIN_SAFE_INTEGER || firstNumber > Number.MAX_SAFE_INTEGER || secondNumber < Number.MIN_SAFE_INTEGER || secondNumber > Number.MAX_SAFE_INTEGER || thirdNumber < Number.MIN_SAFE_INTEGER || thirdNumber > Number.MAX_SAFE_INTEGER) {
+    console.log("\nInvalid.");
+}
+else if (firstNumber<secondNumber && secondNumber<thirdNumber) {
+      console.log("\nStrictly increasing.")
+    }
+    else if (firstNumber<=secondNumber && secondNumber<=thirdNumber) {
+        console.log("\nIncreasing.")
+      }
+  else if (firstNumber==secondNumber && secondNumber==thirdNumber) {
+      console.log("\nEqual.")
+    }
+    else if (firstNumber>=secondNumber && secondNumber>=thirdNumber) {
+        console.log("\nDecreasing.")
+      }
+  else if (firstNumber>secondNumber && secondNumber>thirdNumber) {
+      console.log("\nStrictly decreasing.")
+    }
+  else {
+      console.log("\nUnordered.")
+    }
